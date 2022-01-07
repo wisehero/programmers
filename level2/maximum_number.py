139d47d1a14dddd8c3e17f5fb9da8311f3acf304
김지웅 <godjw96@gmail.com>
2022-01-07 17:48:56 +0900

    Update maximum_number.py

--- level2/maximum_number.py
+++ level2/maximum_number.py
@@ -13,4 +13,12 @@ def solution(numbers):
 
 
 numbers = [6, 10, 2]
-solution(numbers)
+
+
+def optimalSolution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(numbers)))
+
+
+print(optimalSolution(numbers))
