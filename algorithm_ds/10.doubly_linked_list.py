5574bca49e8dec934ca11f561360e2acae647904
김지웅 <godjw96@gmail.com>
2022-02-06 03:30:13 +0900

    Update 10.doubly_linked_list.py

--- algorithm_ds/10.doubly_linked_list.py
+++ algorithm_ds/10.doubly_linked_list.py
@@ -83,3 +83,24 @@ class DoublyLinkedList:
 
         prev = self.getAt(pos - 1)
         return self.insertAfter(prev, newNode)
+
+    def popAfter(self, prev):
+        curr = prev.next
+        prev.next = curr.next
+        curr.next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+    def popBefore(self, next):
+        curr = next.prev
+        prev = curr.prev
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            raise IndexError
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
