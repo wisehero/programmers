fba427c819e09875d3c0f6e9f464383b3af6060a
김지웅 <godjw96@gmail.com>
2022-02-23 01:55:05 +0900

    Create 17.binaryTree.py

---
+++ algorithm_ds/17.binaryTree.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, item):
+        self.data = item
+        self.left = None
+        self.right = None
+
+    def size(self):
+        l = self.left.size() if self.left else 0
+        r = self.right.size() if self.right else 0
+        return l + r + 1
+
+    def depth(self):
+        pass
+
+
+class BinaryTree:
+    def __init__(self, r):
+        self.root = r
