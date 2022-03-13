ed4e5d59b624df372a13632fa7dc994f74567644
김지웅 <godjw96@gmail.com>
2022-03-13 18:43:54 +0900

    Create camouflage.py

---
+++ level2/camouflage.py
@@ -0,0 +1,15 @@
+def solution(clothes):
+    answer = 0
+    cloths = {}
+
+    for name, kind in clothes:
+        if kind in cloths:
+            cloths[kind] += 1
+        else:
+            cloths[kind] = 1
+
+    answer = 1
+    for key, value in cloths.items():
+        answer *= (value + 1)
+
+    return answer - 1  # 모두 안입는 경우 빼기
