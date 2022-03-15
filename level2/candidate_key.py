8c418cde8a93874165fda7f80c03c7fbd4cd87a9
김지웅 <godjw96@gmail.com>
2022-03-15 22:14:44 +0900

    Create candidate_key.py

---
+++ level2/candidate_key.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+relation = [["100", "ryan", "music", "2"],
+            ["200", "apeach", "math", "2"],
+            ["300", "tube", "computer", "3"],
+            ["400", "con", "computer", "4"],
+            ["500", "muzi", "music", "3"],
+            ["600", "apeach", "music", "2"]]
+
+
+
