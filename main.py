ebc4d3996e25d84d639a6dde1951580d840e5109
wisehero10 <godjw96@gmail.com>
2022-11-05 12:38:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-def solution(music):
-    answer = 0
-    now_position = 1
-    black = [2, 4, 6, 9, 11]
-    d = {1: 0, 2: 1, 3: 1, 4: 2, 5: 2, 6: 3, 7: 3, 8: 4, 9: 5, 10: 5, 11: 6, 12: 6}
-    for i in music:
-        if now_position == 1:
-            answer += d[i]
-            now_position = i
-        else:
-            if i == now_position + 1 or i == now_position - 1:
-                answer += 1
-                now_position = i
-            else:
-                if i in black:
-                    answer += abs(d[now_position] - d[i]) + 1
-                    now_position = i
+from collections import Counter
 
-                else:
-                    answer += abs(d[now_position] - d[i])
-                    now_position = i
+
+def solution(line):
+    answer = ''
+    pre = line[0]
+    answer += pre
+    if len(line) == 1:
+        return pre
+    for index in range(1, len(line)):
+        if line[index] == pre:
+            if answer[-1] == '*':
+                continue
+            answer += "*"
+        else:
+            answer += line[index]
+            pre = line[index]
 
     return answer
 
 
-print(solution([10,9,4,5,12]))
+line = "aabbbc"
+print(solution(line))
