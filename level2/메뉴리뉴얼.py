8561174dafe34d2ace31aab0698846acf9c4ec5e
wisehero10 <godjw96@gmail.com>
2022-11-27 02:24:38 +0900

    Create 메뉴리뉴얼.py

---
+++ level2/메뉴리뉴얼.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+from collections import Counter
+
+
+def solution(orders, course):
+    answer = []
+    for k in course:
+        candidates = []
+        for menu_li in orders:
+            for li in combinations(menu_li, k):
+                res = ''.join(sorted(li))
+                candidates.append(res)
+        sorted_candidates = Counter(candidates).most_common()
+        answer += [menu for menu, cnt in sorted_candidates if cnt > 1 and cnt == sorted_candidates[0][1]]
+    return answer
+
+
+solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4])
