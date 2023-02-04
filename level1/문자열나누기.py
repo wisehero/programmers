bed5963e039f80aecd7adf353a3af6d902ba0e6d
wisehero10 <godjw96@gmail.com>
2023-02-05 05:31:41 +0900

    Create 문자열나누기.py

---
+++ level1/문자열나누기.py
@@ -0,0 +1,28 @@
+def solution(s):
+    count_x = 0
+    count_not_x = 0
+    answer = 0
+    x = ''
+    for i in range(len(s)):
+        if x == '':
+            if i == len(s) -1 :
+                answer += 1
+                break
+            x = s[i]
+            count_x += 1
+            continue
+        if s[i] == x:
+            count_x += 1
+        else:
+            count_not_x += 1
+
+        if count_x == count_not_x:
+            x = ''
+            count_x = 0
+            count_not_x = 0
+            answer += 1
+
+    return answer
+
+
+print(solution("aaabbaccccabbb"))
