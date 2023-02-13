23f7112cd1d25576a9f3f07ccf8196362265bdcc
wisehero10 <godjw96@gmail.com>
2023-02-13 20:30:08 +0900

    Create 방문길이.py

---
+++ level2/방문길이.py
@@ -0,0 +1,14 @@
+def solution(dirs):
+    s = set()
+    d = {'U': (0, 1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
+    x, y = 0, 0
+    for dir in dirs:
+        nx, ny = x + d[dir][0], y + d[dir][1]
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            s.add((x, y, nx, ny))
+            s.add((nx, ny, x, y))
+            x, y = nx, ny
+    return len(s) // 2
+
+
+print(solution("LRLRL"))
