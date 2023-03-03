c31cb059bfae7f9c404eff94c2efdc8aea4efc9d
wisehero10 <godjw96@gmail.com>
2023-03-03 17:06:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def solution(array):
-    answer = 0
-    for a in array:
-        a = str(a)
-        answer += a.count("7")
+def solution(bin1, bin2):
+    answer = ''
+    a, b = int(bin1, 2), int(bin2, 2)
+    c = a + b
+    answer = bin(c)[2:]
     return answer
