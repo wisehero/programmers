e502637e361c2baed9d57643ed70e1890602da8c
wisehero10 <godjw96@gmail.com>
2023-03-15 00:09:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-word = input()
-ans = [0] * 10
-for i in range(len(word)):
-    num = int(word[i])
-    if num == 6 or num == 9:
-        if ans[6] <= ans[9]:
-            ans[6] += 1
-        else:
-            ans[9] += 1
-    else:
-        ans[num] += 1
+import heapq
 
-print(max(ans))
+
+def solution(N, coffee_times):
+    if N == 1:
+        return [x for x in range(1, len(coffee_times) + 1)]
+    answer = []
+    q = []
+    stand = 0
+    second = 0
+    while True:
+        while 0 in q:
+            q.remove(0)
+
+        if len(q) < N and stand <= len(coffee_times) - 1:
+            if stand >= N:
+                heapq.heappush(q, (stand, coffee_times[stand] + second))
+                stand += 1
+                continue
+            else:
+                heapq.heappush(q, (stand, coffee_times[stand]))
+                stand += 1
+                continue
+        second += 1
+        for i in range(len(q)):
+            if q[i][1] == second:
+                answer.append(q[i][0] + 1)
+                q[i] = 0
+        if len(coffee_times) == len(answer):
+            break
+    return answer
+
+
+solution(3, [4, 2, 2, 5, 3])
