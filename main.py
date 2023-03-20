7dde626924cad487d9dd16a5a0f51daf7cb08dfc
wisehero10 <godjw96@gmail.com>
2023-03-20 14:03:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-n = int(input())
-for _ in range(n):
-    a, b = map(int, input().split(","))
-    print(a+b)
+li = []
+answer = 0
+for i in range(8):
+    s = input()
+    li.append(list(s))
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 0:
+            if j % 2 == 0 and li[i][j] == "F":
+                answer += 1
+        elif i % 2 != 0:
+            if j % 2 != 0 and li[i][j] == "F":
+                answer += 1
+
+print(answer)
