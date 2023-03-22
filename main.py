647fefd22a13c831bacc5081e28f6d1d173f6dce
wisehero10 <godjw96@gmail.com>
2023-03-22 16:59:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-n = input()
-li = []
-answer = 1
-zero, one = [], []
-tmp = ""
-for i, v in enumerate(list(n)):
-    if tmp != "" and tmp[-1] != v:
-        if tmp[-1] == "1":
-            one.append(tmp)
-            tmp = ""
-        else:
-            zero.append(tmp)
-            tmp = ""
-    tmp += v
-    if i == len(list(n)) - 1:
-        if tmp[-1] == "1":
-            one.append(tmp)
-        else:
-            zero.append(tmp)
+n, m = map(int, input().split())
 
-
-answer = min(len(zero), len(one))
-print(answer)
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = a + b
+c.sort()
+print(*c)
