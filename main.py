a3984382d9daed841e4bf8e18611ac81f2d20f89
wisehero10 <godjw96@gmail.com>
2023-03-31 00:24:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from itertools import product
+from collections import Counter
 
 
-def solution(n):
+def singleNumber(nums):
     answer = 0
-
-    if n == 1:
-        return 1
-
-    for i in range(1, n + 1):
-        li = list(product([1, 2], repeat=i))
-        for l in li:
-            if sum(l) == n:
-                answer += 1
-    return answer
+    counter = Counter(nums).most_common()
+    return counter[-1][0]
 
 
-print(solution(3))
+singleNumber([4, 1, 2, 1, 2])
