0c6ca58b850a0639114afb91d4d0e6f1584d106d
wisehero10 <godjw96@gmail.com>
2023-03-31 01:12:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import math
-
-
-def solution(numer1, denom1, numer2, denom2):
+def solution(n):
     answer = []
-    l = int(lcm(denom1, denom2))
-    n = numer1 * (l // denom1) + numer2 * (l // denom2)
-    answer.append(n)
-    answer.append(l)
+    i = 2
+    while True:
+        if n % i == 0:
+            n //= i
+            answer.append(i)
+        else:
+            i += 1
 
-    while math.gcd(answer[0], answer[1]) != 1:
-        z = math.gcd(answer[0], answer[1])
-        answer[0] = answer[0] // z
-        answer[1] = answer[1] // z
-    return answer
-
-
-def lcm(a, b):
-    for i in range(max(a, b), a * b + 1):
-        if i % a == 0 and i % b == 0:
-            return i
+        if n == 1:
+            break
+    return sorted(list(set(answer)))
 
 
-print(solution(4,4,4,4))
+print(solution(24))
