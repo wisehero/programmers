6b48a69d18a7fa9de11ed2410fed83819cb47703
wisehero10 <godjw96@gmail.com>
2023-03-31 00:36:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from collections import Counter
+import math
 
 
-def singleNumber(nums):
-    answer = 0
-    counter = Counter(nums).most_common()
-    return counter[-1][0]
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    l = int(lcm(denom1, denom2))
+    n = numer1 * (l // denom1) + numer2 * (l // denom2)
+    answer.append(n)
+    answer.append(l)
+
+    while math.gcd(answer[0], answer[1]) != 1:
+        z = math.gcd(answer[0], answer[1])
+        answer[0] = answer[0] // z
+        answer[1] = answer[1] // z
+    return answer
 
 
-singleNumber([4, 1, 2, 1, 2])
+def lcm(a, b):
+    for i in range(max(a, b), a * b + 1):
+        if i % a == 0 and i % b == 0:
+            return i
+
+
+print(solution(4,4,4,4))
