be05c08753df5b3d0cb961e1bf46f07ef7052e27
wisehero10 <godjw96@gmail.com>
2023-03-31 15:22:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-def solution(n):
-    answer = []
-    i = 2
-    while True:
-        if n % i == 0:
-            n //= i
-            answer.append(i)
-        else:
-            i += 1
-
-        if n == 1:
-            break
-    return sorted(list(set(answer)))
+from collections import deque
 
 
-print(solution(24))
+def solution(A, B):
+    if A == B:
+        return 0
+    answer = 0
+    deque_A = deque(A)
+    for _ in range(len(A)):
+        deque_A.appendleft(deque_A.pop())
+        answer += 1
+        if "".join(deque_A) == B:
+            return answer
+        if A == "".join(deque_A):
+            return -1
+    return answer
+
+
+print(solution("hello", "ohell"))
