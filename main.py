182ace1f4c0fa7b6aa34e59abc2a0f2941da96cc
wisehero10 <godjw96@gmail.com>
2023-04-11 18:50:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from collections import Counter
-
-
-def solution(array):
+def solution(board):
     answer = 0
-    counter = Counter(array)
-    if list(counter.values()).count(counter.most_common()[0][1]) > 1:
-        return -1
-    else:
-        return counter.most_common()[0][0]
+    N = len(board)
+    dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+    dy = [0, 0, -1, 1, -1, 1, -1, 1]
+
+    z = []
+
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] == 1:
+                z.append((x, y))
+
+    for x, y in z:
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                board[nx][ny] = 1
+
+    for b in board:
+        answer += b.count(0)
+
+    return answer
+
+
+solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]])
