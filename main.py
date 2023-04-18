4d9bca1df4442ed1c649053f83690de72d5fc470
wisehero10 <godjw96@gmail.com>
2023-04-19 00:04:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-from collections import Counter
-
-
-def solution(kor, usa, incs):
+def solution(n):
     answer = 0
-    same_incs = {}
-
-    for i in kor:
-        for j in usa:
-            same_incs[i + " " + j] = 0
-
-    for key in same_incs.keys():
-        k, u = key.split(" ")
-        for inc in incs:
-            inc = inc.split(" ")
-            if k in inc and u in inc:
-                same_incs[key] += 1
-
-    counter = Counter(same_incs).most_common()[0][1]
-    print(counter)
-    return counter
-
-
-solution(
-    ["AAA", "BCD", "AAAAA", "ZY"],
-    ["AB", "AA", "TTTT"],
-    ["AB BCD AA AAA TTTT AAAAA", "BCD AAA", "AB AAA TTTT BCD", "AA AAAAA AB", "AAA AB BCD"]
-)
-
-solution(
-["CCC", "BCDF"], ["XXXX"], ["BCDF CCC", "XXXX"])
+    for i in range(1, n+1 ):
+        count = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                count += 1
+        if count >= 3:
+            answer += 1
+    return answer
