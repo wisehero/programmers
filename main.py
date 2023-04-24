53b8c5d1d31416ec90fad409a0637a2871f7cd4a
wisehero10 <godjw96@gmail.com>
2023-04-25 00:57:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def solution(n):
-    answer = 0
-    for i in range(1, n+1 ):
-        count = 0
-        for j in range(1, i+1):
-            if i % j == 0:
-                count += 1
-        if count >= 3:
-            answer += 1
-    return answer
+def solution(my_string, is_prefix):
+
+    if my_string.find(is_prefix) == 0:
+        return 1
+    else:
+        return 0
