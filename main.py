73489a218ce2b08d272c7dbc020325c4a2e60580
wisehero10 <godjw96@gmail.com>
2023-04-27 20:09:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def solution(my_string, is_prefix):
+from collections import Counter
 
-    if my_string.find(is_prefix) == 0:
-        return 1
+
+def solution(a, b, c, d):
+    answer = 1
+    li = [a, b, c, d]
+    counter = Counter(li)
+
+    keys = list(counter.keys())
+    if len(keys) == 4:
+        return min(keys)
+    elif len(keys) == 3:
+        for key in keys:
+            if counter[key] == 1:
+                answer *= key
+
+    elif len(keys) == 2:
+        if counter.most_common()[0][1] == 3:
+            return (10 * counter.most_common()[0][0] + counter.most_common()[1][0]) ** 2
+        else:
+            return (keys[0] + keys[1]) * abs(keys[0] - keys[1])
     else:
-        return 0
+        return 1111 * keys[0]
+
+    return answer
