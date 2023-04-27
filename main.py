8916fd7cd53135e27cf37395322a7b48c681b3fc
wisehero10 <godjw96@gmail.com>
2023-04-27 22:04:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-from collections import Counter
-
-
-def solution(a, b, c, d):
-    answer = 1
-    li = [a, b, c, d]
-    counter = Counter(li)
-
-    keys = list(counter.keys())
-    if len(keys) == 4:
-        return min(keys)
-    elif len(keys) == 3:
-        for key in keys:
-            if counter[key] == 1:
-                answer *= key
-
-    elif len(keys) == 2:
-        if counter.most_common()[0][1] == 3:
-            return (10 * counter.most_common()[0][0] + counter.most_common()[1][0]) ** 2
-        else:
-            return (keys[0] + keys[1]) * abs(keys[0] - keys[1])
-    else:
-        return 1111 * keys[0]
-
-    return answer
+def solution(l, r):
+    answer = []
+    for num in range(l, r + 1):
+        if not set(str(num)) - set(['0', '5']):
+            answer.append(num)
+    return answer if answer else [-1]
