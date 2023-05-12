343aa4d52288c56e41abfc8e032a7a37b75f9ed7
wisehero10 <godjw96@gmail.com>
2023-05-12 12:48:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-def solution(l, r):
-    answer = []
-    for num in range(l, r + 1):
-        if not set(str(num)) - set(['0', '5']):
-            answer.append(num)
-    return answer if answer else [-1]
+def solution(flo):
+    return int(flo)
