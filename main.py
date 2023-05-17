8abf2aa3b8d215c2a183b45e1450d5ee0644fc4d
wisehero10 <godjw96@gmail.com>
2023-05-18 04:46:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def solution(flo):
-    return int(flo)
+def solution(order):
+    answer = 0
+    for i in range(len(order)):
+        if "americano" in order[i]:
+            answer += 4500
+        elif "cafelatte" in order[i]:
+            answer += 5000
+        else:
+            answer += 4500
+    return answer
