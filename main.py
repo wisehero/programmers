9b43208d18c5b45a0c554fc2bcaa0deb1e0aab73
wisehero10 <godjw96@gmail.com>
2023-06-13 22:22:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def solution(n):
+def solution(s):
     answer = 0
-    for i in str(n):
-        answer += int(i)
+    cnt1 = 0
+    cnt2 = 0
+    for i in s:
+        if cnt1 == cnt2:
+            answer += 1
+            x = i
+        if i == x:
+            cnt1 += 1
+        else:
+            cnt2 += 1
     return answer
