a73f34177ae9d0ca3c9a88af96996a2fbc8d5ce0
wisehero10 <godjw96@gmail.com>
2023-06-13 22:26:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def solution(s):
-    answer = 0
-    cnt1 = 0
-    cnt2 = 0
-    for i in s:
-        if cnt1 == cnt2:
-            answer += 1
-            x = i
-        if i == x:
-            cnt1 += 1
-        else:
-            cnt2 += 1
+def solution(money):
+    answer = []
+    a, b = divmod(money,5500)
+    answer = [a, b]
     return answer
