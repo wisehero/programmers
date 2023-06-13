f76e05d56dd9500aca2ba5ce2fae8f31f9ea6610
wisehero10 <godjw96@gmail.com>
2023-06-13 20:33:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 def solution(A, B):
-    answer = 0
-    if A == B:
-        return 0
-    A = list(A)
-    for i in range(len(A)):
-        last = A.pop()
-        A.insert(0, last)
-        answer += 1
-        if "".join(A) == B:
-            return answer
-
-        if i == len(A) - 1:
-            return -1
-
-    return answer
+    B *= 2
+    if A in B:
+        return B.find(A)
+    else:
+        return -1
