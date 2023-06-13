05bf6905ba0fc41aea729e5f4e548695ff8761f6
wisehero10 <godjw96@gmail.com>
2023-06-13 20:33:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def solution(my_string, num1, num2):
-    answer = ''
-    my_string = list(my_string)
-    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+def solution(A, B):
+    answer = 0
+    if A == B:
+        return 0
+    A = list(A)
+    for i in range(len(A)):
+        last = A.pop()
+        A.insert(0, last)
+        answer += 1
+        if "".join(A) == B:
+            return answer
 
-    return "".join(my_string)
+        if i == len(A) - 1:
+            return -1
+
+    return answer
