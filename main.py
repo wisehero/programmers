c5be181abc7c21d0071cff5f3b0d4b85eb5815cc
wisehero10 <godjw96@gmail.com>
2023-06-14 21:37:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def solution(money):
-    answer = []
-    a, b = divmod(money,5500)
-    answer = [a, b]
+from itertools import permutations
+
+
+def solution(babbling):
+    can_speak = ["aya", "ye", "woo", "ma"]
+    answer = 0
+
+    for i in range(1, 30):
+
+        for j in list(permutations(can_speak, i)):
+            print(j)
+            if "".join(j) in babbling:
+                answer += 1
+
     return answer
+
+
+solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])
