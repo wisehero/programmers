233b0115dd605a0112d1311bd5d2d9b95383a247
wisehero10 <godjw96@gmail.com>
2023-06-14 21:37:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from itertools import permutations
-
-
-def solution(babbling):
-    can_speak = ["aya", "ye", "woo", "ma"]
-    answer = 0
-
-    for i in range(1, 30):
-
-        for j in list(permutations(can_speak, i)):
-            print(j)
-            if "".join(j) in babbling:
-                answer += 1
+def solution(num, total):
+    nums = [x for x in range(-1000, 1000)]
+    i = 0
+    while True:
+        if sum(nums[i:i + num]) == total:
+            return nums[i:i + num]
+        else:
+            i += 1
 
     return answer
-
-
-solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])
