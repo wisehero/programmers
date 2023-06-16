a962d1d97fdf81d0ea8b294bd1bfc251a5cac0c8
wisehero10 <godjw96@gmail.com>
2023-06-17 00:16:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-def solution(babbling):
-    answer = 0
-    can_speak = ["aya", "ye", "woo", "ma"]
-
-    for i in range(len(babbling)):
-        for s in can_speak:
-            if s in babbling[i]:
-                babbling[i] = babbling[i].replace(s, 'z')
-        print(babbling)
-
-        st = list(set(babbling[i]))
-        if len(st) == 1 and st[0] == "z":
-            answer += 1
-    return answer
-
-
-
-def best_solution(babbling):
-    c = 0
-    for b in babbling:
-        for w in [ "aya", "ye", "woo", "ma" ]:
-            if w * 2 not in b:
-                b = b.replace(w, ' ')
-        if len(b.strip()) == 0:
-            c += 1
-    return c
+import math
+def solution(balls, share):    
+    return math.factorial(balls) // (math.factorial((balls - share)) * math.factorial(share))
