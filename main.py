12cc98abc48ad8ef611790e4f9707f66ebe859aa
wisehero10 <godjw96@gmail.com>
2023-06-17 00:01:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-def solution(arr, flag):
-    answer = []
-    for i in range(len(arr)):
-        if flag[i] == True:
-            for k in range(arr[i] * 2):
-                answer.append(arr[i])
-        else:
-            if len(answer) != 0:
-                for j in range(arr[i]):
-                    answer.pop()
+def solution(babbling):
+    answer = 0
+    can_speak = ["aya", "ye", "woo", "ma"]
+
+    for i in range(len(babbling)):
+        for s in can_speak:
+            if s in babbling[i]:
+                babbling[i] = babbling[i].replace(s, 'z')
+        print(babbling)
+
+        st = list(set(babbling[i]))
+        if len(st) == 1 and st[0] == "z":
+            answer += 1
     return answer
+
+
+
+def best_solution(babbling):
+    c = 0
+    for b in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+            if w * 2 not in b:
+                b = b.replace(w, ' ')
+        if len(b.strip()) == 0:
+            c += 1
+    return c
