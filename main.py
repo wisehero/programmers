fa3b2dc9dcad8a80e78eaf82966cce88de1435a4
wisehero10 <godjw96@gmail.com>
2023-06-17 10:35:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import math
-def solution(balls, share):    
-    return math.factorial(balls) // (math.factorial((balls - share)) * math.factorial(share))
+def solution(hp):
+    answer = 0
+    new_hp = hp
+    while True:
+
+        if 0 < new_hp < 3:
+            answer += new_hp
+            break
+
+        if new_hp // 5 != 0:
+            ants, new_hp = divmod(new_hp, 5)
+            answer += ants
+        else:
+            ants, new_hp = divmod(new_hp, 3)
+            answer += ants
+
+        if new_hp == 0:
+            break
+
+    return answer
+
+
+solution(24)
