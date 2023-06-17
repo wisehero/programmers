8158fc014329315dd2ddab5aebb8c6772a4ddf37
wisehero10 <godjw96@gmail.com>
2023-06-17 10:36:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 def solution(hp):
     answer = 0
-    new_hp = hp
-    while True:
-
-        if 0 < new_hp < 3:
-            answer += new_hp
-            break
-
-        if new_hp // 5 != 0:
-            ants, new_hp = divmod(new_hp, 5)
-            answer += ants
-        else:
-            ants, new_hp = divmod(new_hp, 3)
-            answer += ants
-
-        if new_hp == 0:
-            break
-
+    for ant in [5, 3, 1]:
+        d, hp = divmod(hp, ant)
+        answer += d
     return answer
-
-
-solution(24)
