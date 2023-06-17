110ba0944aec02d8f05ef75567017cf6cff8cbca
wisehero10 <godjw96@gmail.com>
2023-06-17 20:00:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def solution(n):
-    piece = 6
-    while True:
-        if piece // n >= 1 and piece % n == 0:
-            return piece // 6
-        else:
-            piece += 6
+def solution(box, n):
+    answer = 0
 
+    w_count, h_count = box[0] // n, box[1] // n
 
+    semi_result = w_count * h_count
+    answer = semi_result * (box[2] // n)
+
+    return answer
