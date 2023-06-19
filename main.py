91b99023aa556cffc2c2082454355adf07cf976e
wisehero10 <godjw96@gmail.com>
2023-06-20 02:36:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def solution(box, n):
-    answer = 0
-
-    w_count, h_count = box[0] // n, box[1] // n
-
-    semi_result = w_count * h_count
-    answer = semi_result * (box[2] // n)
-
-    return answer
+def solution(num_list, n):
+    if n in num_list:
+        return 1
+    else:
+        return 0 
