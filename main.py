8276ccb08469311eb1e49e0ebc450e64a3c8fb38
wisehero10 <godjw96@gmail.com>
2023-06-21 22:04:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-def solution(chicken):
-    answer = 0
-    coupon = chicken
-
-    service = 0
-
-    while True:
-        service = coupon // 10
-        coupon -= service * 10
-        coupon += service
-        answer += service
-
-        if coupon < 10:
-            break
-
-    return answer
+import math
+def solution(n):
+    for i in range(1, 11):
+        if (i+1) *math.factorial(i) >  n:
+            return i
