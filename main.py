1fabe99e283029195b9e92949bd8541fa6e12c43
wisehero10 <godjw96@gmail.com>
2023-06-22 06:12:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import math
-def solution(n):
-    for i in range(1, 11):
-        if (i+1) *math.factorial(i) >  n:
-            return i
+def solution(sides):
+    answer = 0
+
+    for i in range(1, sides[0] + sides[1]):
+        now = [] + sides
+        now.append(i)
+        now.sort()
+
+        if now[0] + now[1] > now[-1]:
+            answer += 1
+
+    return answer
+
+
+print(solution([3, 6]))
