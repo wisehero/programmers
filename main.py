1099c03adac830e1f086bc4106f9e9eedf359d3c
wisehero10 <godjw96@gmail.com>
2023-06-22 22:38:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from collections import deque
-
-
-def solution(S):
+def minimum_deletion_cost(S, C):
+    z = list(zip(S, C))
     answer = 0
-    # 같을 경우 
-    if len(set(S)) == 1:
-        return len(S)
 
-    q = deque(S)
-    for i in range(len(S) - 1):
-        if q[0] == q[-1]:
-            answer += 1
-        a = q.popleft()
-        q.append(a)
+    for i in range(len(z) - 1):
+        if z[i][0] == z[i+1][0]:
+            answer += min(z[i][1], z[i+1][1])
 
-    return answer
+    print(answer)
 
-
-print(solution("abbaa"))
+# Example usage
+S = "ababa"
+C = [10,5,10,5,10]
+minimum_deletion_cost(S, C)
