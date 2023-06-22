349cce4aab7614404010ef207ef1ce08d2e61c3c
wisehero10 <godjw96@gmail.com>
2023-06-22 22:13:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-def solution(sides):
+from collections import deque
+
+
+def solution(S):
     answer = 0
+    # 같을 경우 
+    if len(set(S)) == 1:
+        return len(S)
 
-    for i in range(1, sides[0] + sides[1]):
-        now = [] + sides
-        now.append(i)
-        now.sort()
-
-        if now[0] + now[1] > now[-1]:
+    q = deque(S)
+    for i in range(len(S) - 1):
+        if q[0] == q[-1]:
             answer += 1
+        a = q.popleft()
+        q.append(a)
 
     return answer
 
 
-print(solution([3, 6]))
+print(solution("abbaa"))
