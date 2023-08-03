0e55e5ecaa8c98db9f60f533ec02092642b25966
wisehero10 <godjw96@gmail.com>
2023-08-04 01:53:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def solution(array):
-    answer = 0
-    for i in array:
-        answer += str(i).count("7")
+def solution(num_list, n):
+    answer = []
+    li = []
+    for i in range(len(num_list)):
+        if (i + 1) % n == 0:
+            li.append(num_list[i])
+            answer.append(li)
+            li = []
+        else:
+            li.append(num_list[i])
     return answer
