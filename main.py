35c51070d5d5b545d652ad073358b8b8e737c0fa
wisehero10 <godjw96@gmail.com>
2023-08-04 02:47:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-def solution(num_list, n):
-    answer = []
-    li = []
-    for i in range(len(num_list)):
-        if (i + 1) % n == 0:
-            li.append(num_list[i])
-            answer.append(li)
-            li = []
-        else:
-            li.append(num_list[i])
+def solution(spell, dic):
+    answer = 0
+
+    for word in dic:
+        b = set(list(word))
+        if set(spell) == b:
+            answer += 1
+
+    if answer == 0:
+        return 2
+    else:
+        return 1
+
     return answer
