91091e9008e6c788a0ebc459c5df2aedbf3223e6
wisehero10 <godjw96@gmail.com>
2023-08-06 23:13:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-t = int(input())
-d = {}
-for _ in range(t):
-    name, record = input().split(" ")
-    d[name] = record
+gpa = 0  # 평점
+hakjum = 0
+d = {
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B0": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D0": 1.0,
+    "F": 0.0
+}
 
-in_company = []
-for key in d.keys():
-    if d[key] == "enter":
-        in_company.append(key)
+for _ in range(20):
+    subject, x, y = input().split(" ")
+    if y != "P":
+        hakjum += float(x)
+        gpa += float(x) * d[y]
 
-in_company.sort(reverse=True)
-
-for emp in in_company:
-    print(emp)
+print(round(gpa / hakjum, 6))
