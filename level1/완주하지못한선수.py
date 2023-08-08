074ff5bf73d98cea883e1860174b06d96594e9eb
wisehero10 <godjw96@gmail.com>
2023-08-08 12:39:13 +0900

    Create 완주하지못한선수.py

---
+++ level1/완주하지못한선수.py
@@ -0,0 +1,20 @@
+import random
+
+
+def solution(participant, completion):
+    answer = ''
+    done = {}
+
+    for key in participant:
+        if key not in done:
+            done[key] = 0
+        else:
+            done[key] -= 1
+
+    for c in completion:
+        if c in done:
+            done[c] += 1
+            
+    for p in participant:
+        if done[p] <= 0:
+            return p
