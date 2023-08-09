9530c821dc0d3b747551d1e18b9b3949fe70a969
wisehero10 <godjw96@gmail.com>
2023-08-09 15:43:47 +0900

    Update printer.py

--- level2/printer.py
+++ level2/printer.py
@@ -6,3 +6,28 @@ J를 대기목록의 가장 마지막에 넣는다.
 '''
 
 
+from collections import deque
+
+
+def solution(priorities, location):
+    answer = 0
+
+    pi = []
+    count = 1
+    for i, p in enumerate(priorities):
+        pi.append([p, i])
+
+    pi = deque(pi)
+    while pi:
+        p, i = pi.popleft()
+
+        if p == max(priorities):
+            if i == location:
+                return count
+            else:
+                count += 1
+            priorities.remove(max(priorities))
+        else:
+            pi.append([p, i])
+
+    return answer
