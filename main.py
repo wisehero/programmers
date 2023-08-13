d1b692e66bbf6149875916414e14d1fad3ac5f8f
wisehero10 <godjw96@gmail.com>
2023-08-14 01:21:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
+import math
+
 n, m = map(int, input().split(" "))
 
-nums = list(map(int, input().split(" ")))
-answer = 0
-
-start = 0
-end = 1
-
-while start <= end <= n:
-    total = sum(nums[start:end])
-
-    if total < m:
-        end += 1
-    elif total > m:
-        start += 1
+maximum = n * m
+result = 0
+while True:
+    if math.ceil(maximum / n) == m:
+        result = maximum
+        maximum -= 1
     else:
-        answer += 1
-        end += 1
-print(answer)
+        break
+
+print(result)
