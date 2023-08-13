af444328d19df2826eabd77c3913c97ffafae816
wisehero10 <godjw96@gmail.com>
2023-08-14 00:25:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-n = int(input())
+n, m = map(int, input().split(" "))
 
-result = []
-for _ in range(n):
-    result.append(int(input()))
+nums = list(map(int, input().split(" ")))
+answer = 0
 
-for i in sorted(result, reverse=True):
-    print(i)
+start = 0
+end = 1
+
+while start <= end <= n:
+    total = sum(nums[start:end])
+
+    if total < m:
+        end += 1
+    elif total > m:
+        start += 1
+    else:
+        answer += 1
+        end += 1
+print(answer)
