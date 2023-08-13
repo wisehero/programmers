03682352fad52ac4e1488c29602433d3f239dc50
wisehero10 <godjw96@gmail.com>
2023-08-13 23:03:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 n = int(input())
 
-cards = [x for x in range(n, 0, -1)]
-left_card = []
+result = []
+for _ in range(n):
+    result.append(int(input()))
 
-flag = True
-while len(cards) != 1:
-    if flag:
-        left_card.append(cards.pop())
-        flag = False
-    else:
-        cards = [cards.pop()] + cards
-        flag = True
-
-for i in left_card:
-    print(i, end=' ')
-print(cards[0])
+for i in sorted(result, reverse=True):
+    print(i)
