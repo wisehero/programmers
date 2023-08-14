dc726d93c4420417afbc7cf656eb85380123c341
wisehero10 <godjw96@gmail.com>
2023-08-15 01:06:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ n, m = map(int, input().split(" "))
 graph = []
 
 for _ in range(n):
-    graph.append(list(input()))
+    graph.append(list(map(int, input().split(" "))))
 
 visited = [[False] * m for _ in range(n)]
+distance = [[0] * m for _ in range(n)]
 direction = [
     [1, 0],
     [-1, 0],
@@ -16,32 +17,33 @@ direction = [
 
 
 def bfs(x, y):
-    global answer
     visited[x][y] = True
+    distance[x][y] = 0
     queue = deque()
-    queue.append([x, y])
+    queue.append([x, y, 0])
 
     while queue:
-        cur_x, cur_y = queue.popleft()
+        cur_x, cur_y, now_position = queue.popleft()
         for i in range(4):
             next_x = cur_x + direction[i][0]
             next_y = cur_y + direction[i][1]
             if 0 <= next_x < n and 0 <= next_y < m:
-                if graph[next_x][next_y] == "P" and not visited[next_x][next_y]:
-                    answer += 1
+                if graph[next_x][next_y] == 1 and not visited[next_x][next_y]:
                     visited[next_x][next_y] = True
-                    queue.append([next_x, next_y])
-                if graph[next_x][next_y] == "O" and not visited[next_x][next_y]:
-                    visited[next_x][next_y] = True
-                    queue.append([next_x, next_y])
+                    distance[next_x][next_y] = now_position + 1
+                    queue.append([next_x, next_y, distance[next_x][next_y]])
 
 
-answer = 0
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == "I":
+        # 출발 지점인 경우 bfs 호출
+        if graph[i][j] == 2:
             bfs(i, j)
-if answer >= 1:
-    print(answer)
-else:
-    print("TT")
+        # 갈 수 있지만 방문되지 않은 경우
+        if graph[i][j] == 1 and not visited[i][j]:
+            distance[i][j] = -1
+
+for i in range(len(distance)):
+    for j in range(len(distance[0])):
+        print(distance[i][j], end=' ')
+    print()
