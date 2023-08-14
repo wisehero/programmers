74c23d879dfcd8a4e954501d40e6933f4ee994a9
wisehero10 <godjw96@gmail.com>
2023-08-15 00:35:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+from collections import deque
+
 n, m = map(int, input().split(" "))
-mA = []
-mB = []
+graph = []
 
-row = max(n, m)
+for _ in range(n):
+    graph.append(list(input()))
 
-for _ in range(row):
-    mA.append(list(map(int, input().split(" "))))
-for _ in range(row):
-    mB.append(list(map(int, input().split(" "))))
+visited = [[False] * m for _ in range(n)]
+direction = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+]
 
+
+def bfs(x, y):
+    global answer
+    visited[x][y] = True
+    queue = deque()
+    queue.append([x, y])
+
+    while queue:
+        cur_x, cur_y = queue.popleft()
+        for i in range(4):
+            next_x = cur_x + direction[i][0]
+            next_y = cur_y + direction[i][1]
+            if 0 <= next_x < n and 0 <= next_y < m:
+                if graph[next_x][next_y] == "P" and not visited[next_x][next_y]:
+                    answer += 1
+                    visited[next_x][next_y] = True
+                    queue.append([next_x, next_y])
+                if graph[next_x][next_y] == "O" and not visited[next_x][next_y]:
+                    visited[next_x][next_y] = True
+                    queue.append([next_x, next_y])
+
+
+answer = 0
 for i in range(n):
     for j in range(m):
-        mA[i][j] += mB[i][j]
-        print(mA[i][j], end=' ')
-    print()
+        if graph[i][j] == "I":
+            bfs(i, j)
+if answer >= 1:
+    print(answer)
+else:
+    print("TT")
