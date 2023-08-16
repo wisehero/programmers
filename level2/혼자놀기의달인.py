65d237d655acdaa5285ba0ae45fe104c9e7a29a5
wisehero10 <godjw96@gmail.com>
2023-08-17 00:05:57 +0900

    Update 혼자놀기의달인.py

--- level2/혼자놀기의달인.py
+++ level2/혼자놀기의달인.py
@@ -1,28 +1,26 @@
 def solution(cards):
-    answer = 0
-    visit = [0 for _ in range(len(cards))]
-    saveCount = []
+    answer = 1
+    group = []
+    visited = [False] * len(cards)
 
-    for index, nowCard in enumerate(cards):
+    for i in range(len(cards)):
+        card = cards[i]
+        if visited[i]:
+            continue
+        visited[i] = True
+        count = 1
 
-        if visit[index] == 0:  # 아직 안열어본 상자라면
-            groupCount = 1  # 상자 1개 카운트
-            visit[index] = 1  # 현재 상자 방문 처리
-            nextIndex = nowCard - 1  # 다음에 열어볼 상자 인덱스
+        while not visited[card - 1]:
+            count += 1
+            visited[card - 1] = True
+            card = cards[card - 1]
 
-            while True:
-                if visit[nextIndex] == 0:  # 다음 상자가 안열어본 상자라면
-                    groupCount += 1
-                    visit[nextIndex] = 1
-                    nextIndex = cards[nextIndex] - 1  # 새로 열어볼 다음 상자
-                else:
-                    break
+        group.append(count)
 
-            saveCount.append(groupCount)
-
-    if len(saveCount) > 1:  # 그룹이 2개 이상이라면
-        answer = saveCount[0] * saveCount[1]
+    group.sort(reverse=True)
+    if len(group) > 1:
+        return group[0] * group[1]
     else:
-        answer = 0
+        return 0
 
     return answer
