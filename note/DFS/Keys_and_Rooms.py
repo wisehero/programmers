cb04f73b2ea66ad47908be6ff31d64430ba93f97
wisehero10 <godjw96@gmail.com>
2023-08-17 15:56:49 +0900

    Create Keys_and_Rooms.py

---
+++ note/DFS/Keys_and_Rooms.py
@@ -0,0 +1,18 @@
+rooms = [[1, 3], [2, 4], [0], [4], [], [3, 4]]
+
+
+def canVisitAllRooms(rooms):
+    visited = set()
+
+    def dfs(v):
+        visited.add(v)
+        for next_v in rooms[v]:
+            if next_v in visited:
+                dfs(next_v)
+
+    dfs(0)
+
+    if len(visited) == len(rooms):
+        return True
+    else:
+        return False
