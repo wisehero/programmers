875cef9397adefb07382590a3dbd1a3d70aa033c
wisehero10 <godjw96@gmail.com>
2023-09-03 11:55:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,19 @@
-from collections import defaultdict, deque
+n = int(input())
+m = int(input())
+s = input()
 
-n, m = map(int, input().split())
-
-dic = dict()
-result = dict()
+dp = [0]
 for i in range(1, n + 1):
-    dic[i] = []
+    if i == 1:
+        dp.append("IOI")
+    else:
+        dp.append(dp[i-1] + "OI")
 
-print(dic)
-for _ in range(m):
-    key, friend = map(int, input().split(" "))
-    if friend not in dic[key] and key not in dic[friend]:
-        dic[key].append(friend)
-        dic[friend].append(key)
+pn = dp[n]
+len_pn = len(pn)
+result = 0
+for i in range(len(s)):
+    if s[i:i + len_pn] == pn:
+        result += 1
 
-
-def bfs(start):
-    visited = [start]
-    count = 0
-    point = 0
-    q = deque()
-    q.append([start, point])
-
-    while q:
-        cur_node, point = q.popleft()
-        for v in dic[cur_node]:
-            if v not in visited:
-                count += point + 1
-                visited.append(v)
-                q.append([v, point + 1])
-    return count
-
-
-for key in dic.keys():
-    result[key] = bfs(key)
-
-r = sorted(result.items(), key=lambda x: (x[1], x[0]))
-print(r)
-print(r[0][0])
+print(result)
