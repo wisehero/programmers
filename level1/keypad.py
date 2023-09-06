d3551be0072ff6f60c7edd23df7485342210156c
wisehero10 <godjw96@gmail.com>
2023-09-06 14:09:55 +0900

    Update keypad.py

--- level1/keypad.py
+++ level1/keypad.py
@@ -1,37 +1,45 @@
-import math
-
-
 def solution(numbers, hand):
     answer = ''
-    left_hand = [1, 4, 7]
-    right_hand = [3, 6, 9]
-    ruled_hand = [2, 5, 8, 0]
-    left_p = [(3, 0)]
-    right_p = [(3, 2)]
+
+    keypad = {
+        1: [1, 1],
+        2: [1, 2],
+        3: [1, 3],
+        4: [2, 1],
+        5: [2, 2],
+        6: [2, 3],
+        7: [3, 1],
+        8: [3, 2],
+        9: [3, 3],
+        "*": [4, 1],
+        0: [4, 2],
+        "#": [4, 3]
+    }
+
+    left_hand = keypad["*"]
+    right_hand = keypad["#"]
+
     for num in numbers:
-        # 1, 4, 7을 누를 때
-        if num in left_hand:
+        if num in [1, 4, 7]:
             answer += "L"
-            left_p.append((left_hand.index(num), 0))
-        # 3, 6, 9를 누를 때
-        if num in right_hand:
+            left_hand = keypad[num]
+        elif num in [3, 6, 9]:
             answer += "R"
-            right_p.append((right_hand.index(num), 2))
-        # 가운데 2, 5, 8, 0을 누를 때,
-        if num in ruled_hand:
-            num_position = (ruled_hand.index(num), 1)
-            # 두 점 사이의 거리를 구했을 때 더 작은 쪽에 있는 손으로
-            if math.ceil(math.dist(left_p[-1], num_position)) < math.ceil(math.dist(right_p[-1], num_position)):
+            right_hand = keypad[num]
+        else:
+            rd = abs(right_hand[1] - keypad[num][1]) + abs(right_hand[0] - keypad[num][0])
+            ld = abs(left_hand[1] - keypad[num][1]) + abs(left_hand[0] - keypad[num][0])
+            if ld < rd:
                 answer += "L"
-                left_p.append((ruled_hand.index(num), 1))
-            elif math.ceil(math.dist(left_p[-1], num_position)) > math.ceil(math.dist(right_p[-1], num_position)):
+                left_hand = keypad[num]
+            elif ld > rd:
                 answer += "R"
-                right_p.append((ruled_hand.index(num), 1))
-            # 두 점 사이의 거리가 같을 때는 주로 사용하는 손으로
+                right_hand = keypad[num]
             else:
-                answer += hand[0].upper()
                 if hand == "right":
-                    right_p.append((ruled_hand.index(num), 1))
-                if hand == "left":
-                    left_p.append((ruled_hand.index(num), 1))
+                    answer += "R"
+                    right_hand = keypad[num]
+                else:
+                    answer += "L"
+                    left_hand = keypad[num]
     return answer
