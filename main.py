15adaade437024b0be079b89f82bf587f9ae4dd3
wisehero10 <godjw96@gmail.com>
2023-09-07 22:41:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-def solution(keymap, targets):
+# 모든 달은 28일까지 있다.
+def solution(today, terms, privacies):
     answer = []
+    terms_dic = {}
+    for term in terms:
+        a, b = term.split(" ")
+        terms_dic[a] = int(b) * 28
 
-    for target in targets:
-        t = True
-        amount = 0
-        for i in range(len(target)):
-            count = 999
-            for j in range(len(keymap)):
-                if target[i] in keymap[j]:
-                    count = min(keymap[j].index(target[i]) + 1, count)
-                    t = True
-                else:
-                    t = False
-            amount += count
+    year, month, day = today.split(".")
 
-        if t:
-            answer.append(amount)
-        else:
-            answer.append(-1)
+    today = int(day) + (int(month) * 28) + (int(year) * 28 * 12)
+
+    for privacy in privacies:
+        date, term = privacy.split(" ")
+        date_year, date_month, date_day = date.split(".")
+
+        date_time = int(date_day) + (int(date_month) * 28) + (int(date_year) * 28 * 12)
+        date_time += terms_dic[term]
+
+        if date_time < today:
+            answer.append(privacies.index(privacy) + 1)
 
     return answer
 
-
-solution(["AGZ", "BSSS"], ["ASA","BGZ"])
+solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])
