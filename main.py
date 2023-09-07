f4b74074641567d148cc98ac3be786e318a2eb77
wisehero10 <godjw96@gmail.com>
2023-09-08 00:03:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-# 모든 달은 28일까지 있다.
-def solution(today, terms, privacies):
-    answer = []
-    terms_dic = {}
-    for term in terms:
-        a, b = term.split(" ")
-        terms_dic[a] = int(b) * 28
+from itertools import combinations
 
-    year, month, day = today.split(".")
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    saram = list(input().split())
+    if n > 32:
+        print(0)
+    else:
+        answer = []
+        cases = list(combinations(saram, 3))
 
-    today = int(day) + (int(month) * 28) + (int(year) * 28 * 12)
+        for case in cases:
+            distance = 0
+            for i in range(2):
+                for j in range(i + 1, 3):
+                    distance += len(set(case[i]).difference(case[j]))
+            answer.append(distance)
 
-    for privacy in privacies:
-        date, term = privacy.split(" ")
-        date_year, date_month, date_day = date.split(".")
-
-        date_time = int(date_day) + (int(date_month) * 28) + (int(date_year) * 28 * 12)
-        date_time += terms_dic[term]
-
-        if date_time < today:
-            answer.append(privacies.index(privacy) + 1)
-
-    return answer
-
-solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])
+        print(min(answer))
