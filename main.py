6d9e16bd964d3f7ea4a43bc16181885541159a5b
wisehero10 <godjw96@gmail.com>
2023-09-17 00:43:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-def solution(x, n):
-    answer = []
+from collections import Counter
 
-    num = x
 
-    while n > 0:
-        answer.append(num)
-        num += x
-        n -= 1
-    return answer
+def solution(array):
+    answer = 0
+
+    if len(array) == 1:
+        return array[0]
+
+    counter = Counter(array)
+
+    c_list = counter.most_common(2)
+
+    if len(c_list) >= 2 and c_list[0][1] == c_list[1][1]:
+        return -1
+
+    return c_list[0][0]
+
+
+
