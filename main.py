e24a2c58eedcb37b302e0aad9148c11055f79d61
wisehero10 <godjw96@gmail.com>
2023-09-17 00:42:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-def solution(hp):
-    answer = 0
+def solution(x, n):
+    answer = []
 
-    while hp:
-        if hp >= 5:
-            answer += hp // 5
-            hp %= 5
-        elif 3 <= hp < 5:
-            answer += hp // 3
-            hp %= 3
-        else:
-            answer += hp
-            hp %= 1
+    num = x
 
+    while n > 0:
+        answer.append(num)
+        num += x
+        n -= 1
     return answer
