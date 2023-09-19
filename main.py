e013c7270d697421684dfd0cc7a897b39732e0d8
wisehero10 <godjw96@gmail.com>
2023-09-19 13:40:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from collections import Counter
-
-
-def solution(array):
-    answer = 0
-
-    if len(array) == 1:
-        return array[0]
-
-    counter = Counter(array)
-
-    c_list = counter.most_common(2)
-
-    if len(c_list) >= 2 and c_list[0][1] == c_list[1][1]:
-        return -1
-
-    return c_list[0][0]
-
-
-
+def solution(n):
+    n = list(str(n))
+    n.sort(reverse=True)
+    return int("".join(n))
