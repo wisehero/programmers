81e7a55b95da13ef3fdfd37e69f4c0c7ebea433e
wisehero10 <godjw96@gmail.com>
2023-09-19 14:20:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def solution(n):
-    n = list(str(n))
-    n.sort(reverse=True)
-    return int("".join(n))
+def solution(x):
+    answer = True
+    str_x = str(x)
+    n = 0
+
+    for s in str_x:
+        n += int(s)
+
+    if x % n != 0:
+        answer = False
+    return answer
