2b976738724d7275585751f51dd0cbc1ae32b9b3
wisehero10 <godjw96@gmail.com>
2023-09-23 17:58:36 +0900

    Update k번째수.py

--- level1/k번째수.py
+++ level1/k번째수.py
@@ -3,7 +3,7 @@ def solution(array, commands):
 
     for i in range(len(commands)):
         a, b, c = commands[i]
-        new_array = array[a-1: b]
+        new_array = array[a - 1: b]
         new_array.sort()
-        answer.append(new_array[c-1])
+        answer.append(new_array[c - 1])
     return answer
