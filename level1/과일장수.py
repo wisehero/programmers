c0d1c6005f377abf5ddda45a34f258db85789260
wisehero10 <godjw96@gmail.com>
2023-09-29 19:22:46 +0900

    Update 과일장수.py

--- level1/과일장수.py
+++ level1/과일장수.py
@@ -1,16 +1,11 @@
 def solution(k, m, score):
     answer = 0
     score.sort()
-    score = score[::-1]
     box = []
-    for i in range(len(score)):
-        box.append(score[i])
+    while score:
+        box.append(score.pop())
         if len(box) == m:
             answer += min(box) * m
             box = []
-    if len(box) < m:
-        return answer
 
-
-score = [1, 2, 3, 1, 2, 3, 1]
-print(solution(3, 4, score))
+    return answer
