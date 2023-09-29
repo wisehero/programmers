442f7cdb46aa59b1363e8112671ea7b2bb910bbb
wisehero10 <godjw96@gmail.com>
2023-09-29 20:23:12 +0900

    Update 문자열나누기.py

--- level1/문자열나누기.py
+++ level1/문자열나누기.py
@@ -1,25 +1,25 @@
 def solution(s):
-    count_x = 0
-    count_not_x = 0
     answer = 0
-    x = ''
-    for i in range(len(s)):
-        if x == '':
-            if i == len(s) - 1:
-                answer += 1
-                break
-            x = s[i]
-            count_x += 1
-            continue
-        if s[i] == x:
-            count_x += 1
-        else:
-            count_not_x += 1
 
-        if count_x == count_not_x:
-            x = ''
-            count_x = 0
-            count_not_x = 0
+    is_x = 0
+    is_not_x = 0
+    i = 0
+    x = s[i]
+    while True:
+        if i == len(s) - 1:
             answer += 1
+            break
+        if s[i] == x:
+            is_x += 1
+        else:
+            is_not_x += 1
+
+        i += 1
+
+        if is_x == is_not_x:
+            answer += 1
+            x = s[i]
+            is_x = 0
+            is_not_x = 0
 
     return answer
