d784444da54fd2d9d1b8e17f1fb7b635c8a6cfe6
wisehero10 <godjw96@gmail.com>
2023-10-03 17:20:55 +0900

    Update 완주하지못한선수.py

--- level1/완주하지못한선수.py
+++ level1/완주하지못한선수.py
@@ -1,20 +1,14 @@
-import random
+from collections import Counter
 
 
 def solution(participant, completion):
     answer = ''
-    done = {}
 
-    for key in participant:
-        if key not in done:
-            done[key] = 0
-        else:
-            done[key] -= 1
+    pc = Counter(participant)
 
     for c in completion:
-        if c in done:
-            done[c] += 1
+        pc[c] -= 1
 
-    for p in participant:
-        if done[p] <= 0:
-            return p
+    for k in pc.keys():
+        if pc[k] > 0:
+            return k
