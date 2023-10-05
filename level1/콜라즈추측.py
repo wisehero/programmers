900b8c1ff7271fc341132f72c5e95d332498b6e6
wisehero10 <godjw96@gmail.com>
2023-10-05 18:21:27 +0900

    Update 콜라즈추측.py

--- level1/콜라즈추측.py
+++ level1/콜라즈추측.py
@@ -1,17 +1,15 @@
 def solution(num):
     answer = 0
-    target = num
-    while True:
-        if target == 1:
-            break;
-        if target % 2 == 0:
-            target //= 2
-            answer += 1
-        else:
-            target = target * 3 + 1
-            answer += 1
+    if num == 0:
+        return 0
+
+    while num != 1:
         if answer == 500:
             return -1
+        if num % 2 == 0:
+            num /= 2
+            answer += 1
+        else:
+            num = num * 3 + 1
+            answer += 1
     return answer
-
-print(solution(6))
