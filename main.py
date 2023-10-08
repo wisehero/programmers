bb4ac0b0ae0cc27ec1e6294a68ec75211c54ef99
wisehero10 <godjw96@gmail.com>
2023-10-08 20:06:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-def solution(k, score):
+def solution(park, routes):
     answer = []
-    rank = []
+    obs = []
 
-    for i in range(len(score)):
-        rank.append(score[i])
-        rank.sort()
+    start = []
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j] == "S":
+                start.append(i)
+                start.append(j)
+            elif park[i][j] == "X":
+                obs.append([i, j])
 
-        if len(rank) < k:
-            answer.append(min(rank))
-        else:
-            answer.append(min(rank[-k:]))
+    for r in routes:
+        d, m = r.split(" ")
+        m = int(m)
+
+        if d == "N" and 0 <= start[0] - m < len(park):
+            origin = start[0]
+            for i in range(m):
+                start[0] -= 1
+                if start in obs:
+                    start[0] = origin
+                    break
+        elif d == "S" and 0 <= start[0] + m < len(park):
+            origin = start[0]
+            for i in range(m):
+                start[0] += 1
+                if start in obs:
+                    start[0] = origin
+                    break
+        elif d == "E" and 0 <= start[1] + m < len(park[0]):
+            origin = start[1]
+            for i in range(m):
+                start[1] += 1
+                if start in obs:
+                    start[1] = origin
+                    break
+        elif d == "W" and 0 <= start[1] - m < len(park[0]):
+            origin = start[1]
+            for i in range(m):
+                start[1] -= 1
+                if start in obs:
+                    start[1] = origin
+                    break
+
+    answer = start
 
     return answer
+
+solution(["OSO", "OOO", "OXO", "OOO"], ["E 2", "S 3", "W 1"])
