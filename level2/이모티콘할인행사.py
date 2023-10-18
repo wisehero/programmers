6619d176343ad91c021a9e4bab9863ae8319d624
wisehero10 <godjw96@gmail.com>
2023-10-18 11:26:22 +0900

    Update 이모티콘할인행사.py

--- level2/이모티콘할인행사.py
+++ level2/이모티콘할인행사.py
@@ -1,33 +1,33 @@
-# 우선 이모티콘 플러스 가입자를 늘린다.
-# 그 다음 이모티콘 판매액을 늘린다.
-from itertools import combinations_with_replacement, permutations, product
+from itertools import product
 
 
 def solution(users, emoticons):
     answer = []
-    result = []
     discount = [0.4, 0.3, 0.2, 0.1]
-    discount_comb = list(product(discount, repeat=len(emoticons)))
-    for comb in discount_comb:
-        plus_user = 0
-        emoticon_paid = 0
-        dis_target = emoticons.copy()
-        for i in range(len(dis_target)):
-            dis_target[i] *= 1 - comb[i]
+    discount_prod = list(product(discount, repeat=len(emoticons)))
+
+    # 가능한 할인율 조합
+    for prod in discount_prod:
+        plus_user = 0 # 플러스 유저 수
+        emoticon_paid = 0 # 이모티콘 구입액
+        discount_emo = emoticons.copy()
+        print(discount_emo)
+        for i in range(len(discount_emo)):
+            discount_emo[i] *= 1 - prod[i]
+
         for user in users:
             paid_price = 0
             percent = user[0] / 100
-            for i in range(len(dis_target)):
-                if comb[i] >= percent:
-                    paid_price += dis_target[i]
-
+            for i in range(len(discount_emo)):
+                if prod[i] >= percent:
+                    paid_price += discount_emo[i]
             if paid_price >= user[1]:
                 plus_user += 1
             else:
                 emoticon_paid += paid_price
-        result.append((plus_user, int(emoticon_paid)))
-    result = sorted(result, key=lambda x: (-x[0], -x[1]))
-    print(result)
+        answer.append((plus_user, int(emoticon_paid)))
+
+    result = sorted(answer, key=lambda x: (-x[0], -x[1]))
     return list(result[0])
 
 
