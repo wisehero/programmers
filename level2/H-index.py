2696533ab677785b240d48d73fd9b725aa04d82d
wisehero10 <godjw96@gmail.com>
2023-10-25 15:04:51 +0900

    Update H-index.py

--- level2/H-index.py
+++ level2/H-index.py
@@ -1,19 +1,14 @@
 def solution(citations):
-    answer = 0
-    n = len(citations)
-    for i in range(n - 1, -1, -1):  # 끝에서 부터 출발
-        h = i + 1
-        cnt_use = 0
-        cnt_notuse = 0
-        for j in citations:
-            if j <= h:
-                cnt_notuse += 1
-            if j >= h:
-                cnt_use += 1
-        if cnt_use >= h and cnt_notuse <= h:
-            answer = h
-            break
-    return answer
+    i = 10000
+    while True:
+        up = 0
+        down = 0
+        for citation in citations:
+            if citation >= i:
+                up += 1
+            else:
+                down += 1
 
-
-solution([3, 0, 6, 1, 5])
+        if up >= i >= down:
+            return i
+        i -= 1
