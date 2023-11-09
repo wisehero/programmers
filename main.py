dc57495a8f8e8c88197609db740a4a0c8eea06f0
wisehero10 <godjw96@gmail.com>
2023-11-10 01:38:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
-from collections import Counter
-
 n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(input())
+arr = list(map(int, input().split()))
 
-c = Counter(arr)
-c = list(c.items())
-c.sort(key=lambda x: (-x[1], x[0]))
-print(c[0][0])
+switch = []
+for i in range(len(arr)):
+    switch.append([i + 1, arr[i]])
+
+person = int(input())
+people = []
+
+for _ in range(person):
+    a, b = map(int, input().split())
+    people.append([a, b])
+
+for a, b in people:
+    if a == 1:
+        for i in range(len(switch)):
+            if switch[i][0] % b == 0:
+                if switch[i][1] == 1:
+                    switch[i][1] = 0
+                else:
+                    switch[i][1] = 1
+
+    if a == 2 and len(switch) >= 3:
+
+        start = 0
+        end = 0
+        selection = b - 1
+        i = 1
+
+        if selection == 0 or selection == len(switch) - 1:
+            if switch[selection][1] == 1:
+                switch[selection][1] = 0
+            else:
+                switch[selection][1] = 1
+
+            continue
+        while switch[selection - i][1] == switch[selection + i][1]:
+            start = selection - i
+            end = selection + i
+            i += 1
+            if start == 0 or end == len(switch) - 1:
+                break
+
+        if 0 <= start < end:
+            for i in range(start, end + 1):
+                if switch[i][1] == 0:
+                    switch[i][1] = 1
+                else:
+                    switch[i][1] = 0
+        else:
+            if switch[selection][1] == 1:
+                switch[selection][1] = 0
+            else:
+                switch[selection][1] = 1
+
+for i in range(len(switch)):
+    print(switch[i][1], end=" ")
+    if i % 20 == 19:
+        print()
