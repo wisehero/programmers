609b7881556ce0d75a60c2d3e9c45ed719b672ed
wisehero10 <godjw96@gmail.com>
2023-11-16 20:23:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from collections import Counter
+n = int(input())
 
-word = input()
-word = word.upper()
-c = Counter(word)
-c = list(c.items())
-c.sort(key=lambda x: -x[1])
-
-if len(c) >= 2 and c[0][1] == c[1][1]:
-    print("?")
-else:
-    print(c[0][0].upper())
+for i in range(1, n + 1):
+    s = " " * (n - i) + "*" * i
+    print(s)
