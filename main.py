bbddd1392285ffac7fda5a436a1481e2950c6a2d
wisehero10 <godjw96@gmail.com>
2023-11-18 00:47:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 n = int(input())
 
-for i in range(1, n + 1):
-    s = str(i)
-    a = i
+arr = []
 
-    for j in range(len(s)):
-        a += int(s[j])
+for _ in range(n):
+    w, h = map(int, input().split())
+    arr.append([w, h])
 
-    if a == n:
-        print(i)
-        break
+ans = []
+for i in range(len(arr)):
+    w1, h1 = arr[i][0], arr[i][1]
+    rank = 1
+    for j in range(len(arr)):
+        if i == j:
+            continue
+        w2, h2 = arr[j][0], arr[j][1]
+        if w1 < w2 and h1 < h2:
+            rank += 1
+    ans.append(rank)
 
-print(0)
+print(*ans)
