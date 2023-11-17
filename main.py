7a4b8fed8c46e457ac82fde09334d7d59b77c99d
wisehero10 <godjw96@gmail.com>
2023-11-18 01:39:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-n = int(input())
+import sys
+from collections import deque
 
-room = 1
-ans = 1
-while room < n:
-    room += 6 * ans
-    ans += 1
+t = int(sys.stdin.readline())
+q = deque()
 
-print(ans)
+while t:
+    command = sys.stdin.readline().rstrip()
+
+    if command == "front":
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif command == "back":
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
+    elif command == "pop_front":
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif command == "pop_back":
+        if q:
+            print(q.pop())
+        else:
+            print(-1)
+    elif command == "empty":
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif command == "size":
+        print(len(q))
+    else:
+        c, n = command.split(" ")
+        if c == "push_back":
+            q.append(n)
+        else:
+            q.appendleft(n)
+
+    t -= 1
