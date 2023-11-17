120257a4295029e7d98260eb4b7e353fc5e8ccc3
wisehero10 <godjw96@gmail.com>
2023-11-18 01:20:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 n = int(input())
 
-arr = []
+room = 1
+ans = 1
+while room < n:
+    room += 6 * ans
+    ans += 1
 
-for _ in range(n):
-    w, h = map(int, input().split())
-    arr.append([w, h])
-
-ans = []
-for i in range(len(arr)):
-    w1, h1 = arr[i][0], arr[i][1]
-    rank = 1
-    for j in range(len(arr)):
-        if i == j:
-            continue
-        w2, h2 = arr[j][0], arr[j][1]
-        if w1 < w2 and h1 < h2:
-            rank += 1
-    ans.append(rank)
-
-print(*ans)
+print(ans)
