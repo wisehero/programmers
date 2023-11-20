3966abe1e2e2ad8994c44a69baf7e1217504d5a6
wisehero10 <godjw96@gmail.com>
2023-11-21 00:29:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-n = int(input())
+n, k = map(int, input().split())
 
-target_arr = []
 arr = []
 for _ in range(n):
-    target_arr.append(int(input()))
-target_arr = target_arr[::-1]
+    arr.append(int(input()))
+
+arr = arr[::-1]
+
 i = 0
-ans = []
+ans = 0
+while k != 0:
+    ans += k // arr[i]
+    k %= arr[i]
+    i += 1
 
-for i in range(1, n + 1):
-    arr.append(i)
-    ans.append("+")
-    while arr and arr[-1] == target_arr[-1]:
-        arr.pop()
-        target_arr.pop()
-        ans.append("-")
-
-if len(arr) != 0:
-    print("NO")
-else:
-    for a in ans:
-        print(a)
+print(ans)
