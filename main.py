dc19ef6e0864c622f4caa31ab2b7355ea51c1b5f
wisehero10 <godjw96@gmail.com>
2023-11-20 22:17:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 n = int(input())
-numbers = list(map(int, input().split()))
-d = {}
-for num in numbers:
-    if num in d:
-        d[num] += 1
-    else:
-        d[num] = 1
-ans = []
-m = int(input())
-m_arr = list(map(int, input().split()))
-for a in m_arr:
-    if a in d:
-        ans.append(d[a])
-    else:
-        ans.append(0)
 
-print(*ans)
+target_arr = []
+arr = []
+for _ in range(n):
+    target_arr.append(int(input()))
+target_arr = target_arr[::-1]
+i = 0
+ans = []
+
+for i in range(1, n + 1):
+    arr.append(i)
+    ans.append("+")
+    while arr and arr[-1] == target_arr[-1]:
+        arr.pop()
+        target_arr.pop()
+        ans.append("-")
+
+if len(arr) != 0:
+    print("NO")
+else:
+    for a in ans:
+        print(a)
