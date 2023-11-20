8d1d0e98ba89918c1ac78de31c749ad82530c5c1
wisehero10 <godjw96@gmail.com>
2023-11-20 22:01:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-while True:
-    string = input()
-    if string == ".":
-        break
-    if string[-1] != ".":
-        print("no")
-        continue
-
-    string = list(string)
-    a = ''
-    while string:
-        s = string.pop(0)
-        if s in "()[]":
-            a += s
-
-        if "()" in a:
-            a = a.replace("()", "")
-            if "[]" in a:
-                a = a.replace("[]", "")
-        if "[]" in a:
-            a = a.replace("[]", "")
-            if "()" in a:
-                a = a.replace("()", "")
-
-    if a.strip() == '':
-        print("yes")
+n = int(input())
+numbers = list(map(int, input().split()))
+d = {}
+for num in numbers:
+    if num in d:
+        d[num] += 1
     else:
-        print("no")
+        d[num] = 1
+ans = []
+m = int(input())
+m_arr = list(map(int, input().split()))
+for a in m_arr:
+    if a in d:
+        ans.append(d[a])
+    else:
+        ans.append(0)
+
+print(*ans)
