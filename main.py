bb0a752f6c71ef85e106bc600a972a75de1e8b73
wisehero10 <godjw96@gmail.com>
2023-11-22 20:13:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from collections import deque
 
-t = int(input())
+n, m = map(int, input().split())
 
-for _ in range(t):
-    n, m = map(int, input().split())
+maps = []
+for _ in range(n):
+    maps.append(list(input()))
 
-    arr = list(map(int, input().split()))
-    q = []
-    for i in range(len(arr)):
-        q.append([i, arr[i]])
+visited = [[False] * m for _ in range(n)]
+distance = [[0] * m for _ in range(n)]
 
-    q = deque(q)
 
-    priority = sorted(arr)
+def bfs(row, col):
+    visited[row][col] = True
 
-    cnt = 0
+    q = deque()
+    cnt = 1
+    distance[row][col] = cnt
+    q.append([row, col, cnt])
+    direction = [[0, 1], [1, 0], [-1, 0], [0, -1]]
     while q:
-        p = q.popleft()
-        if p[1] == priority[-1]:
-            priority.pop()
-            cnt += 1
-            if p[0] == m:
-                print(cnt)
-                break
-        else:
-            q.append(p)
+        cur_row, cur_col, cnt = q.popleft()
+
+        for i in range(4):
+            next_row = cur_row + direction[i][0]
+            next_col = cur_col + direction[i][1]
+
+            if 0 <= next_row < n and 0 <= next_col < m and maps[next_row][next_col] == "1":
+                if not visited[next_row][next_col]:
+                    visited[next_row][next_col] = True
+                    q.append([next_row, next_col, cnt + 1])
+                    distance[next_row][next_col] = cnt + 1
+
+
+for i in range(n):
+    for j in range(m):
+        if maps[i][j] == "1" and not visited[i][j]:
+            bfs(i, j)
+print(distance[n-1][m-1])
