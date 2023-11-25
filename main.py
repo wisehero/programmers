3f8b54ce6b6731b3b04ce5f8d339ca31c5cadf72
wisehero10 <godjw96@gmail.com>
2023-11-25 15:03:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-num = [0] * 10001
+def solution(friends, gifts):
+    m = len(friends)
+    answer = [0] * m
+    arr = [[0] * m for _ in range(m)]
+    d = {}
+    present = {}
+    for i in range(m):
+        present[i] = [0, 0]
+    for friend in friends:
+        d[friend] = friends.index(friend)
 
-for i in range(1, 10000):
-    n = i
+    for gift in gifts:
+        give, receivce = gift.split(" ")
+        i = d[give]
+        j = d[receivce]
+        if i != j:
+            arr[i][j] += 1
+        present[i][0] += 1
+        present[j][1] += 1
 
-    for j in str(i):
-        n += int(j)
+    for i in range(m):
+        for j in range(m):
+            if arr[i][j] > arr[j][i]:
+                answer[i] += 1
 
-    if n < len(num):
-        num[n] += 1
+    print(answer)
+    for i in range(m):
+        for j in range(i, m):
+            if arr[i][j] == arr[j][i]:
+                if present[i][0] - present[i][1] < present[j][0] - present[j][1]:
+                    answer[j] += 1
+                elif present[i][0] - present[i][1] > present[j][0] - present[j][1]:
+                    answer[i] += 1
 
-for i in range(1, 10001):
-    if num[i] == 0:
-        print(i)
+    return answer
+
+
+print(solution(
+
+    ["joy", "brad", "alessandro", "conan", "david"],
+    ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"]))
