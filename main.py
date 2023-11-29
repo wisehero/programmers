04e9f31fac765f968dff22b4116546c13dafd781
wisehero10 <godjw96@gmail.com>
2023-11-29 23:04:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-import sys
+n, m = map(int, input().split())
 
-sys.setrecursionlimit(10 ** 6)
-n, m, r = map(int, sys.stdin.readline().split())
+a = set()
 
-graph = [[] for _ in range(n + 1)]
+for _ in range(n):
+    a.add(input())
 
+ans = 0
 for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    b = input()
+    if b in a:
+        ans += 1
 
-visited = [0] * (n + 1)
-cnt = 1
-
-
-def dfs(graph, v, visited):
-    global cnt
-    visited[v] = cnt
-
-    for i in graph[v]:
-        if visited[i] == 0:
-            cnt += 1
-            dfs(graph, i, visited)
-
-
-for i in range(n + 1):
-    graph[i].sort()
-
-dfs(graph, r, visited)
-
-for i in range(1, n + 1):
-    print(visited[i])
+print(ans)
