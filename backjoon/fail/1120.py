d89922c951c7c6e9c0d5a37517a65ab2c2d29e2e
wisehero10 <godjw96@gmail.com>
2023-12-10 20:39:06 +0900

    Update 1120.py

--- backjoon/fail/1120.py
+++ backjoon/fail/1120.py
@@ -1,6 +1,6 @@
 a, b = input().split()
 result = []
-for i in range(len(b) - len(a) - 1):
+for i in range(len(b) - len(a) + 1):
     cnt = 0
     for j in range(len(a)):
         if a[j] != b[i + j]:
