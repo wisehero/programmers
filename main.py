6318e134eb5043fae0a324b959015fae6ec0e955
wisehero10 <godjw96@gmail.com>
2023-12-19 01:31:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 t = int(input())
 
 for i in range(1, t + 1):
-    ans = 0
-    n = int(input())
-    for k in range(1, n + 1):
-        if k % 2 == 0:
-            ans -= k
-        else:
-            ans += k
+    s = set()
+    start = int(input())
+    a = start
+    while True:
+        for e in str(start):
+            s.add(e)
 
-    print(f"#{i} {ans}")
+        if s == {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}:
+            break
+        start += a
+    print(f"#{i} {start}")
