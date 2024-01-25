be330e3c9b76e1fdb11bf6d84ef6bbc65d2a044b
wisehero10 <godjw96@gmail.com>
2024-01-26 03:25:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import math
+# 최소 한 개의 모음과 최소 두 개의 자음으로 구성되어 있다.
+# 문자열은 정렬되어있어야한다.
+from itertools import combinations
 
-n, m = map(int, input().split())
+l, c = map(int, input().split())
+arr = list(input().split())
 
-print(math.factorial(n) // (math.factorial(n - m) * math.factorial(m)))
+v = {"a", "i", "e", "o", "u"}
+not_v = set("bcdfghjklmnpqrstvwxyz")
+
+answer = []
+
+cases = list(combinations(arr, l))
+for case in cases:
+    if len(set(case).intersection(v)) >= 1 and len(set(case).intersection(not_v)) >= 2:
+        case = sorted(case)
+        password = "".join(case)
+        answer.append(password)
+
+answer.sort()
+for i in answer:
+    print(i)
