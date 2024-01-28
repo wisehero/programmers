71ec76ce88fd74f445b0ae57258ffea62cff9939
wisehero10 <godjw96@gmail.com>
2024-01-29 08:39:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-n = int(input())
+a, b = input().split()
 
-if n < 100:
-    print(n)
-    exit()
+if len(a) == len(b):
+    answer = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            answer += 1
+else:
+    answer = 99999
 
-answer = 99
-
-for i in range(100, n + 1):
-    k = str(i)
-    m = int(k[len(k) // 2])
-    a, b = int(k[0]), int(k[-1])
-
-    if (a + b) / 2 == m:
-        answer += 1
+    for i in range(len(b) - len(a) + 1):
+        temp = 0
+        s = b[i:i + len(a)]
+        for j in range(len(s)):
+            if a[j] != s[j]:
+                temp += 1
+        answer = min(answer, temp)
 
 print(answer)
