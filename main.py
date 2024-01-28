eaeda6fe1d6ab559c9f9e4de2699cb689ff16910
wisehero10 <godjw96@gmail.com>
2024-01-28 13:43:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-# A와 B의 길이가 다르면, 짧은 것이 먼저 온다.
-# 길이가 서로 같다면 A의 모든 자리수의 합과 B의 모든 자리수의 합을 비교해서 작은 합을 가지는 것이 먼저 온다.
-# 사전 순으로 한다.
+# 회의는 한번 시작하면 중간에 중단될 수 없다.
+# 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+
 n = int(input())
 arr = []
 
 for _ in range(n):
-    arr.append([input(), 0])
+    start, end = map(int, input().split())
+    arr.append([start, end])
 
-for i in range(len(arr)):
-    s = 0
-    for j in arr[i][0]:
-        if j.isdigit():
-            s += int(j)
-    arr[i][1] = s
-arr.sort(key=lambda x: (len(x[0]), x[1], x[0]))
+arr.sort(key=lambda x: (x[1], x[0]))
+
+answer = 0
+start, end = 0, 0
 for a in arr:
-    print(a[0])
+    if end <= a[0]:
+        start, end = a[0], a[1]
+        answer += 1
+
+print(answer)
