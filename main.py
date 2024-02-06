56506d142896c164968994150098a8061f27a13c
wisehero10 <godjw96@gmail.com>
2024-02-06 20:50:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-n = int(input())
-arr = []
+n, m = map(int, input().split())
 
-for _ in range(n):
-    arr.append(input())
+s1 = set(input().split())
+s2 = set(input().split())
 
-answer = 0
-for a in arr:
-    a = list(a)
-    stack = []
-    while a:
-        stack.append(a.pop())
-        # 스택의 길이가 2 이상이고 스택의 맨 마지막과 그 전에 요소가 같으면 반복
-        while len(stack) >= 2 and stack[-1] == stack[-2]:
-            stack.pop()
-            stack.pop()
-    # 스택이 비어있으면 좋은 단어
-    if not stack:
-        answer += 1
-
-print(answer)
+print(len(s1.difference(s2)) + len(s2.difference(s1)))
